04d424a6825f39a9fa6b03cbd018446f158b1f0e
wasifstarzed <khanwasifmohd@gmail.com>
2021-12-19 21:57:49 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello this is First Deploye
+          NiDA HeLLO dEAr
         </p>
         <a
           className="App-link"
